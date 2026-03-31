33745a030bc20c7e4ff95b7775d491d4e65aa763
Norven Ephraim Caracas <58012641+D3c1pher@users.noreply.github.com>
2026-03-31 14:55:35 +0800

    feat: display monster UI with image, name, and HP on spawn

--- Assets/Test Tasks/Editable/ClientMobsManager.cs
+++ Assets/Test Tasks/Editable/ClientMobsManager.cs
@@ -1,10 +1,18 @@
 using TestTask.NonEditable;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace TestTask.Editable
 {
     public class ClientMobsManager : MonoBehaviour
     {
+        [SerializeField] private Image monsterImage;
+        [SerializeField] private TextMeshProUGUI monsterNameText;
+        [SerializeField] private Slider monsterHpBar;
+
+        [SerializeField] private Sprite[] monsterSprites;
+
         private MonsterData currentMonster;
 
         public void OnMonsterSpawn(MonsterData monster)
@@ -13,14 +21,38 @@ namespace TestTask.Editable
 
             Debug.Log($"Monster spawned: ID={monster.MonsterId}, Type={monster.MonsterType}, HP={monster.MonsterCurrentHealth}/{monster.MonsterMaxHealth}");
 
+            UpdateMonsterUI();
+
+            currentMonster.MonsterDamaged += OnMonsterDamaged;
             currentMonster.MonsterDeath += OnMonsterDeath;
+        }
+
+        private void UpdateMonsterUI()
+        {
+            monsterImage.sprite = monsterSprites[(int)currentMonster.MonsterType];
+
+            monsterNameText.text = currentMonster.MonsterName;
+
+            monsterHpBar.minValue = 0f;
+            monsterHpBar.maxValue = 1f;
+            monsterHpBar.value = currentMonster.MonsterCurrentHealth / currentMonster.MonsterMaxHealth;
+        }
+
+        private void OnMonsterDamaged(float hpRatio)
+        {
+            monsterHpBar.value = hpRatio;
         }
 
         private void OnMonsterDeath()
         {
             Debug.Log($"Monster died: ID={currentMonster.MonsterId}, Type={currentMonster.MonsterType}");
 
+            currentMonster.MonsterDamaged -= OnMonsterDamaged;
             currentMonster.MonsterDeath -= OnMonsterDeath;
+
+            monsterImage.sprite = null;
+            monsterNameText.text = string.Empty;
+            monsterHpBar.value = 0f;
 
             currentMonster = null;
         }
